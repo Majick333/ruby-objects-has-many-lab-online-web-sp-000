d55813b457fcffd48b96948fd9d2e868bb93d493
Ryan M <rmccauleymusic@gmail.com>
2019-11-17 23:04:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,33 @@
 class Author
-  attr_accessor :name, :posts
+  attr_accessor :name
+
+  @@post_count = 0
 
     def initialize(name)
       @name = name
       @posts = []
+      @post_count +=1
     end
 
     def posts
       @posts
     end
 
-    def add_post(posts)
-      @all << posts
+    def add_post(post)
+      self.post << post
+      post.author = self
+      @@post_count +=1
     end
 
+    def add_post_by_title(title)
+      post = Post.new(title)
+      self.posts << post
+      post.author = self
+      @@post_count +=1
+    end
+
+    def post_count
+      @@post_count
+    end
 
 end
