40eb0a7fb432bc06badbe7dd4777c398d3a77b70
Ryan M <rmccauleymusic@gmail.com>
2019-11-17 22:24:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,14 @@ attr_accessor :name, :songs, :song_count
     song.artist = self
   end
 
-  def songs
-    @songs
+  def song
+    @song
   end
+
+  def songs
+    song.all.select { |song| song.artist == self  }
+  end
+
 
   def self.song_count
     @songs.length
