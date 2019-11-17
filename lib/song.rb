6e1c4fcfb40cb854897b38bf8d0ca8e373263b90
Ryan M <rmccauleymusic@gmail.com>
2019-11-17 21:51:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 attr_accessor :name, :artist
 @@all = []
 
-  def initialize(name,artist_name=nil)
+  def initialize(name, artist_name=nil)
     @name = name
     @@all << self.name
     @artist_name = @artist
